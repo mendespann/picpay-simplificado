8b14c8f333d285bd34501442721a3eba7092a4e3
Pamela Mendes <pammendesr@ufu.br>
2024-05-22 19:19:13 -0300

    Adicionando to dos

---
+++ src/main/java/br/com/picpay/payment/domain/ports/EmailPort.java
@@ -0,0 +1,5 @@
+package br.com.picpay.payment.domain.ports;
+
+public interface EmailPort {
+  //TODO implementar serviço de notificacao por email
+}
---
+++ src/main/java/br/com/picpay/payment/infrastructure/adapters/EmailAdapter.java
@@ -0,0 +1,7 @@
+package br.com.picpay.payment.infrastructure.adapters;
+
+import br.com.picpay.payment.domain.ports.EmailPort;
+
+public class EmailAdapter implements EmailPort {
+  //TODO implementar serviço de notificacao por email
+}
